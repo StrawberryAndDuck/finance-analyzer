4122ad0e639335ae38bd9a48eef338215f09abb1
Ryu0n <hsung951027@gmail.com>
2023-12-16 21:00:06 +0900

    u: chat complete 인터페이스 작성

--- app/services/gpt/service.py
+++ app/services/gpt/service.py
@@ -1,7 +1,7 @@
 import json
-from app.utils.util_path import get_repository_path
-
 from openai import OpenAI
+
+from app.utils.util_path import get_repository_path
 
 
 def init_openai_client():
@@ -9,3 +9,13 @@ def init_openai_client():
     with open(repo_path.joinpath("app/services/slack/secrets/secret.json"), "r") as f:
         secret = json.load(f)
         return OpenAI(api_key=secret.get("bot-token"))
+
+
+def query_with_prompt(client: OpenAI, model: str, prompt: str):
+    return client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt},
+        ],
+    )
