42659812543ba10aef47753a570d78849f0d1b43
Ryu0n <hsung951027@gmail.com>
2025-01-27 17:06:18 +0900

    u: 실행 대상 경로 수정정

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 DEFAULT_PORT=8080
 
 export $(grep -v '^#' .env | xargs)
-gunicorn --workers 1 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$DEFAULT_PORT -m 007 app.api.main:app --timeout 0 --keep-alive 5
+gunicorn --workers 1 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$DEFAULT_PORT -m 007 finance.main:app --timeout 0 --keep-alive 5
